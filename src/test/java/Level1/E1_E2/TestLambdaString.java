49a13cd1815ca0df07e1a14a8f86098a71d36d9b
crist <cristhianchulca49@gmail.com>
2025-12-02 12:10:57 +0100

    test: add testGetStringWithOAndLengthMoreThanFive checking strings containing 'o' and length > 5

--- src/test/java/Level1/E1_E2/TestLambdaString.java
+++ src/test/java/Level1/E1_E2/TestLambdaString.java
@@ -1,17 +1,29 @@
 package Level1.E1_E2;
 
 import Level1.LambdaString;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestLambdaString {
+    //Exercise 1
     @Test
-    public void testGetStringWith() {
+    public void testGetStringWithO() {
         List<String> list = List.of("Cristian", "JavaScript", "Book", "Mouse", "Sun", "Saturday");
         List<String> expected = List.of("Book", "Mouse");
-        List<String> result = LambdaString.getStringWith(list);
-        Assert.assertEquals(expected, result);
+        List<String> result = LambdaString.getStringWithO(list);
+        assertEquals(expected, result);
     }
+
+    //Exercise 2
+    @Test
+    public void testGetStringWithOAndLengthMoreThanFive() {
+        List<String> list = List.of("Cristian", "Neighbourhood", "Book", "Mouse", "Pedro", "Notebook");
+        List<String> expected = List.of("Neighbourhood", "Notebook");
+        List<String> result = LambdaString.getStringWithOAndLengthMoreThanFive(list);
+        assertEquals(expected, result);
+    }
+
 }
