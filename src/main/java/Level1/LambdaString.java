68367d415c11fe6d59ad51b0cc4987ba95a84838
crist <cristhianchulca49@gmail.com>
2025-12-02 12:09:28 +0100

    feat: add getStringWithOAndLengthMoreThanFive method to filter strings containing 'o' and length > 5

--- src/main/java/Level1/LambdaString.java
+++ src/main/java/Level1/LambdaString.java
@@ -1,6 +1,7 @@
 package Level1;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LambdaString {
@@ -16,5 +17,13 @@ public class LambdaString {
 
     public static void printList(List<String> list) {
         list.forEach(System.out::println);
+    }
+
+    public static List<String> getStringWithOAndLengthMoreThanFive(List<String> list) {
+        List<String> result = list.stream()
+                .filter(l -> l.toLowerCase().contains("o") && l.length() > 5)
+                .toList();
+        printList(result);
+        return result;
     }
 }
