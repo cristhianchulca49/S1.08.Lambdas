d909055bd4f3ffe8a68bcea95bef7426e8dc42ba
crist <cristhianchulca49@gmail.com>
2025-12-02 13:44:28 +0100

    feat: add sortReverseObjectsByLength method to sort objects by string length in descending order

--- src/main/java/Level1/E6/SortByLength.java
+++ src/main/java/Level1/E6/SortByLength.java
@@ -5,9 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SortByLength {
+    //Exercise 1
     public static List<Object> sortObjectsByLength(List<Object> list) {
         return list.stream()
                 .sorted(Comparator.comparing(l -> l.toString().length()))
                 .collect(Collectors.toList());
+    }
+
+    //Exercise 2
+    public static List<Object> sortReverseObjectsByLength(List<Object> list) {
+        return list.stream()
+                .sorted(Comparator.comparing(l -> l.toString().length()))
+                .collect(Collectors.toList())
+                .reversed();
     }
 }
