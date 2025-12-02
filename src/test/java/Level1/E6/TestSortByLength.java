c00c046092eb976f4a0a8e92f4252f5990cc214b
crist <cristhianchulca49@gmail.com>
2025-12-02 13:35:47 +0100

    test: add testSortObjectsByLength to verify sorting by string length

--- src/test/java/Level1/E6/TestSortByLength.java
+++ src/test/java/Level1/E6/TestSortByLength.java
@@ -1,4 +1,18 @@
 package Level1.E6;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 public class TestSortByLength {
+    @Test
+    public void testSortObjectsByLength() {
+        List<Object> list = List.of("1,2,3,4,5", "Patrice", 12345, "12345", "numbers", "0", 0, 123456789);
+        List<Object> expected = List.of("0", 0, 12345, "12345", "Patrice", "numbers", "1,2,3,4,5", 123456789);
+        List<Object> result = SortByLength.sortObjectsByLength(list);
+
+        assertEquals(expected, result);
+    }
 }
