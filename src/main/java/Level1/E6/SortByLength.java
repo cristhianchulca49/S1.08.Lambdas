f714cb4459acd3afd23ac2deedb5700862e96a48
crist <cristhianchulca49@gmail.com>
2025-12-02 13:21:13 +0100

    feat: add SortObjectsByLength method using streams to return a new sorted List<Object>

--- src/main/java/Level1/E6/SortByLength.java
+++ src/main/java/Level1/E6/SortByLength.java
@@ -1,5 +1,13 @@
 package Level1.E6;
 
-public class SortByLength {
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class SortByLength {
+    public static List<Object> sortObjectsByLength(List<Object> list) {
+        return list.stream()
+                .sorted(Comparator.comparing(l -> l.toString().length()))
+                .collect(Collectors.toList());
+    }
 }
