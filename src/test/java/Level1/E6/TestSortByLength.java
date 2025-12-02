ec20e45e271fd2895e41ba2e4c26ad8bfba2ec13
crist <cristhianchulca49@gmail.com>
2025-12-02 13:44:40 +0100

    test: add testSortReverseObjectsByLength to verify sorting objects by string length in reverse

--- src/test/java/Level1/E6/TestSortByLength.java
+++ src/test/java/Level1/E6/TestSortByLength.java
@@ -15,4 +15,13 @@ public class TestSortByLength {
 
         assertEquals(expected, result);
     }
+
+    @Test
+    public void testSortReverseObjectsByLength() {
+        List<Object> list = List.of("1,2,3,4,5", "Patrice", 12345, "12345", "numbers", "0", 0, 123456789);
+        List<Object> expected = List.of(123456789, "1,2,3,4,5", "numbers", "Patrice", "12345", 12345, 0, "0");
+        List<Object> result = SortByLength.sortReverseObjectsByLength(list);
+
+        assertEquals(expected, result);
+    }
 }
