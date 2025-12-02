a7aab59c8a4c9db5468f71db0f26bf1dc826ac35
crist <cristhianchulca49@gmail.com>
2025-12-02 12:24:23 +0100

    feat: main Class to test

---
+++ src/main/java/Level1/E3/Main.java
@@ -0,0 +1,10 @@
+package Level1.E3;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> year = List.of("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        Year.printYear(year);
+    }
+}
