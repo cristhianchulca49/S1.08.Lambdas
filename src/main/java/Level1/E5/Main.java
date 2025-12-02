fbfd42bcc8a3184774f0ccc61699a194e4836903
crist <cristhianchulca49@gmail.com>
2025-12-02 14:36:38 +0100

    refactor: create PI final attribute

--- src/main/java/Level1/E5/Main.java
+++ src/main/java/Level1/E5/Main.java
@@ -2,7 +2,8 @@ package Level1.E5;
 
 public record Main() {
     public void main(String[] args) {
-        PiInterface pi = () -> 3.1415;
+        final double PI = 3.1415;
+        PiInterface pi = () -> PI;
         System.out.println(pi.getPiValue());
     }
 }
