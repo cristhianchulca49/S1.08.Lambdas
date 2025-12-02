dee3fbfd27db6a2bea795a0cbc8fc01d61108843
crist <cristhianchulca49@gmail.com>
2025-12-02 11:22:52 +0100

    test: add testGetStringWith to verify filtering of strings with 'o'

--- src/test/java/Level1/E1_E2/TestLambdaString.java
+++ src/test/java/Level1/E1_E2/TestLambdaString.java
@@ -1,10 +1,17 @@
 package Level1.E1_E2;
 
+import Level1.LambdaString;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TestLambdaString {
     @Test
     public void testGetStringWith() {
-
+        List<String> list = List.of("Cristian", "JavaScript", "Book", "Mouse", "Sun", "Saturday");
+        List<String> expected = List.of("Book", "Mouse");
+        List<String> result = LambdaString.getStringWith(list);
+        Assert.assertEquals(expected, result);
     }
 }
