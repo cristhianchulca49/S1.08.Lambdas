09037543da258e7cdf312ee3162a6c41fe238e73
crist <cristhianchulca49@gmail.com>
2025-12-02 11:06:52 +0100

    feat: add E1_E2 for exersice 1 and 2

---
+++ src/main/java/Level1/LambdaString_E1_E2.java
@@ -0,0 +1,16 @@
+package Level1;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaString_E1_E2 {
+    //Exercise 1
+    private List<String> mainList = List.of("Book", "Pedro", "Lambdas", "JavaScript", "Cristian", "Open");
+
+    public static List<String> getStringWith(List<String> list) {
+        return list.stream()
+                .filter(l -> l.toLowerCase().contains("o"))
+                .collect(Collectors.toList());
+    }
+}
